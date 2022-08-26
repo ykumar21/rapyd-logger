db797adff516db45392a183ea764a9010a2a77e8
Yash <yash20008@gmail.com>
2022-08-26 14:05:33 +0800

    Added getter and setter for fd and child_pid

--- src/rapyd/Process.h
+++ src/rapyd/Process.h
@@ -2,11 +2,17 @@
 #define PROCESS_H
 
 #include <optional> // For optional<?>
+#include <stdio.h> 
+
+typedef const char* c_str;
 
 /* Process for logging */ 
 class Process {
 private:
-    const char* process_name; 
+    c_str   process_name;
+    int     fd[2];
+    pid_t   childpid;    
+
     std::optional<const char*> create_fifo_pipe();
     void read_from_pipe(int);
     void write_to_pipe(int);
@@ -17,6 +23,21 @@ public:
     }
     void _register();
 
+    int* get_fd() {
+        return this->fd;
+    }
+
+    void set_fd(int *fd) {
+        fprintf(stderr, "Dont set fd.. Call create_fifo_pipe");
+    }
+
+    pid_t get_childpid() {
+        return this->childpid;
+    }
+
+    void set_childpid() {
+        fprintf(stderr, "Dont set pid.. call create_fifo_pipe");
+    }
 };
 
 #endif
